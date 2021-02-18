05e03db3ab3616d7c46a549d4f199e93720a2529
1Dimitri <git.dimitri@janczak.net>
2021-02-18 14:47:47 +0100

    Create Out-IniFile.ps1

---
+++ INI/Out-IniFile.ps1
@@ -0,0 +1,25 @@
+unction Out-IniFile($InputObject, $FilePath)
+{
+    $outFile = New-Item -ItemType file -Path $Filepath
+    foreach ($i in $InputObject.keys)
+    {
+        if (!($($InputObject[$i].GetType().Name) -eq “Hashtable”))
+        {
+            #No Sections
+            Add-Content -Path $outFile -Value “$i=$($InputObject[$i])”
+        } else {
+            #Sections
+            Add-Content -Path $outFile -Value “[$i]”
+            Foreach ($j in ($InputObject[$i].keys | Sort-Object))
+            {
+                if ($j -match “^Comment[\d]+”) {
+                    Add-Content -Path $outFile -Value “$($InputObject[$i][$j])”
+                } else {
+                    Add-Content -Path $outFile -Value “$j=$($InputObject[$i][$j])”
+                }
+
+            }
+            Add-Content -Path $outFile -Value “”
+        }
+    }
+}
